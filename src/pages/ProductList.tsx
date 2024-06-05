f81caaefe578e108d92e9b4c24a963a466ea9c62
Sarika <sarika.goooogle.1330@gmail.com>
2024-06-05 12:57:13 +0530

    updated ui

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -47,85 +47,104 @@ const ProductList: React.FC = () => {
   };
 
   return (
-    <div className="w-[90%] m-auto">
-      <SearchBar Value={searchQuery} OnChange={handleSearchChange} />
-      {filteredProducts.length > 0 ? (
-        <div className="p-1 m-auto grid grid-cols-1 auto-rows-auto lg:grid-cols-4 md:grid-cols-2">
-          {filteredProducts?.map((el, index) => {
-            return (
-              <Link to={`/product/${el.id}`} key={el.id}>
-                <div
-                  
-                  className="bg-white flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-lg group p-2"
-                >
-                  <svg
-                    className="absolute bottom-0 left-0 mb-8 scale-150 group-hover:scale-[1.65] transition-transform"
-                    viewBox="0 0 375 283"
-                    fill="none"
-                    style={{ opacity: "0.1" }}
-                  >
-                    <rect
-                      x="159.52"
-                      y="175"
-                      width="152"
-                      height="152"
-                      rx="8"
-                      transform="rotate(-45 159.52 175)"
-                      fill={index % 2 === 0 ? "blue" : "red"}
-                    />
-                    <rect
-                      y="107.48"
-                      width="152"
-                      height="152"
-                      rx="8"
-                      transform="rotate(-45 0 107.48)"
-                      fill={index % 2 === 0 ? "green" : "yellow"}
-                    />
-                  </svg>
-                  <div className="relative pt-10 px-10 flex items-center justify-center group-hover:scale-110 transition-transform">
-                    <div
-                      className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-                      style={{
-                        background: "radialGradient(black, transparent 60%)",
-                        transform:
-                          "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
-                        opacity: "0.2",
-                      }}
-                    ></div>
-                    <span className="block opacity-75 -mb-1 text-black font-bold text-lg pl-2">
-                      {el.title}
-                    </span>
-                    <img className="relative w-40" src={el.thumbnail} alt="" />
-                  </div>
-                  <div className="relative px-6 pb-6 mt-6">
-                    <div className="flex justify-between">
-                      <span className="block text-black">{el.category}</span>
-                      <span className=" bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
-                        $ {el.price}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            );
-          })}
+    <>
+      {productStatus ? (
+        <div className="mt-5 flex justify-center items-center">
+          <div
+            className="m-auto inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-info motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
         </div>
       ) : (
-        <div className="w-3/4 m-auto ">
-          <img
-            className="m-auto"
-            src="https://www.lifecoachhub.com/files/images/no-product-found.png"
-            alt="No Product"
+        <div className="w-[90%] m-auto">
+          <SearchBar Value={searchQuery} OnChange={handleSearchChange} />
+          {filteredProducts.length > 0 ? (
+            <div className="p-1 m-auto grid grid-cols-1 auto-rows-auto lg:grid-cols-4 md:grid-cols-2">
+              {filteredProducts?.map((el, index) => {
+                return (
+                  <Link to={`/product/${el.id}`} key={el.id}>
+                    <div className="bg-white flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-lg group p-2">
+                      <svg
+                        className="absolute bottom-0 left-0 mb-8 scale-150 group-hover:scale-[1.65] transition-transform"
+                        viewBox="0 0 375 283"
+                        fill="none"
+                        style={{ opacity: "0.1" }}
+                      >
+                        <rect
+                          x="159.52"
+                          y="175"
+                          width="152"
+                          height="152"
+                          rx="8"
+                          transform="rotate(-45 159.52 175)"
+                          fill={index % 2 === 0 ? "blue" : "red"}
+                        />
+                        <rect
+                          y="107.48"
+                          width="152"
+                          height="152"
+                          rx="8"
+                          transform="rotate(-45 0 107.48)"
+                          fill={index % 2 === 0 ? "green" : "yellow"}
+                        />
+                      </svg>
+                      <div className="relative pt-10 px-10 flex items-center justify-center group-hover:scale-110 transition-transform">
+                        <div
+                          className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                          style={{
+                            background:
+                              "radialGradient(black, transparent 60%)",
+                            transform:
+                              "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                            opacity: "0.2",
+                          }}
+                        ></div>
+                        <span className="block opacity-75 -mb-1 text-black font-bold text-lg pl-2">
+                          {el.title}
+                        </span>
+                        <img
+                          className="relative w-40"
+                          src={el.thumbnail}
+                          alt=""
+                        />
+                      </div>
+                      <div className="relative px-6 pb-6 mt-6">
+                        <div className="flex justify-between">
+                          <span className="block text-black">
+                            {el.category}
+                          </span>
+                          <span className=" bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
+                            $ {el.price}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          ) : (
+            <div className="w-3/4 m-auto ">
+              <img
+                className="m-auto"
+                src="https://www.lifecoachhub.com/files/images/no-product-found.png"
+                alt="No Product"
+              />
+            </div>
+          )}
+
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
           />
         </div>
       )}
-
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
-    </div>
+    </>
   );
 };
 
--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
 import { getSProduct } from "../redux/productSlice";
+import { FiStar } from "react-icons/fi";
+import { url } from "inspector";
 
 export const SingleProduct: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -17,64 +19,72 @@ export const SingleProduct: React.FC = () => {
     (state: RootState) => state.products.selectedProduct
   );
 
-  if (!id) {
-    return <div className="container mx-auto p-4">Invalid product ID</div>;
+  if (!id||!selectedProduct) {
+    return <div className="container mx-auto p-4">
+    <div className="w-3/4 m-auto ">
+          <img
+            className="m-auto"
+            src="https://www.lifecoachhub.com/files/images/no-product-found.png"
+            alt="No Product"
+          />
+        </div>
+        <Link to="/"><h3 className="text-center text-lg mt-4 font-bold text-cyan-800">Back To Home Page</h3></Link>
+    </div>;
   }
-  // console.log(selectedProduct)
 
   return (
-    <div className="w-full p-4 mt-4">
-      <div className="w-3/4 m-auto mt-6 py-6 shadow-shadBo bg-cyan-100 rounded-xl">
-     
-
+    <div className="w-full p-4 mt-4 ">
+      <div className=" w-3/4 m-auto mt-6 py-6 shadow-shadBo rounded-xl bg-cover bg-center" style={{backgroundImage:"url(https://img6.thuthuatphanmem.vn/uploads/2022/03/15/mau-background-dep-cho-card-visit_092710350.png)"}}>
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="relative pt-10 px-10 flex items-center justify-center group-hover:scale-110 transition-transform">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex flex-col md:flex-row -mx-4">
                 <div className="md:flex-1 px-4">
-                  <div className="relative h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4 hover:scale-110 transition ease-in-out duration-150">
-                    <img
-                      className="w-full h-full object-cover border-2 rounded-lg opacity-75 hover:opacity-100 "
-                      src={selectedProduct?.thumbnail}
-                      alt="Product"
-                    />
-                    <div
-                      className="absolute w-full bottom-[5%] p-0 opacity-50"
-                      style={{
-                        backgroundColor:
-                          selectedProduct?.availabilityStatus === "In Stock"
-                            ? "green"
-                            : "red",
-                      }}
+                  
+                  <div
+            className="overflow-hidden cursor-pointer rounded-xl relative group"
+        >
+       
+            <img
+                
+                className="object-cover w-full h-full aspect-square group-hover:scale-110 transition duration-300 ease-in-out"
+                src={selectedProduct?.thumbnail} alt="pro"/>
+                <div
+                      className="absolute w-1/5 bottom-[5%] p-0 opacity-70 text-black rounded-xl"
+                      
                     >
-                      {"...."}
+                      {selectedProduct?.availabilityStatus === "In Stock"?<img className="w-3/4 m-auto" src="https://cdn-icons-png.flaticon.com/512/5278/5278681.png" alt="available"/>:<img src="https://cdn-icons-png.flaticon.com/512/5278/5278580.png" alt="outofstock"/>}
                     </div>
-                  </div>
+        
+        </div>
                   <div className="flex -mx-2 mb-4 mt-4">
                     <div className="w-1/2 px-2 m-auto mt-3">
                       <Link to={`/product/${selectedProduct?.id}/edit`}>
-                      <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
-                        Edit
-                      </button>
+                        <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+                          Edit
+                        </button>
                       </Link>
                     </div>
                   </div>
                 </div>
-            
-                  
+
                 <div className="md:flex-1 px-4">
                   <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-2">
                     {selectedProduct?.title}
                   </h2>
-                  <p className="text-gray-600text-sm">
-                    {selectedProduct?.category}
+                  <p className="text-gray-600 text-md">
+                    {`(${selectedProduct?.category})`}
                   </p>
-                      {selectedProduct?.brand?<p className="text-gray-600 text-lg font-bold">
-                    Brand: {selectedProduct?.brand}
-                  </p>:""}
-                  <span className="text-gray-600 rounded-lg mt-2 p-1.5 dark:text-gray-300 text-sm font-bold mb-1 bg-yellow-200">
-                    Rating: {selectedProduct?.rating}
-                  </span>
+                  {selectedProduct?.brand ? (
+                    <p className="text-gray-600 text-lg font-bold">
+                      Brand: {selectedProduct?.brand}
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                  <div className="w-2/5 flex justify-start text-grey-600 rounded-lg mt-2 p-1.5 dark:text-gray-300 text-sm font-bold mb-1">
+                    <FiStar className="mt-1"/> <h5> Rating: {selectedProduct?.rating} </h5> <FiStar className="mt-1"/>
+                  </div>
                   <div className="p-3">
                     {selectedProduct?.tags?.map((el) => (
                       <span
@@ -85,7 +95,7 @@ export const SingleProduct: React.FC = () => {
                       </span>
                     ))}
                   </div>
-                  
+
                   <div className="flex mb-4">
                     <div className="mr-4">
                       <span className="font-bold text-gray-700 dark:text-gray-300">
@@ -109,17 +119,17 @@ export const SingleProduct: React.FC = () => {
                       Dimensions (w/h/d) (in cm):
                     </span>
                     <div className="flex items-center mt-2">
-                      <div className="w-9 h-8 p-1 flex justify-center items-center bg-red-500 dark:bg-red-700 mr-2 text-sm font-semi-bold">
+                      <div className="w-9 h-8 p-1 flex justify-center items-center bg-red-500 mr-2 rounded-lg text-sm font-semi-bold hover:scale-125 tranisition ease-in-out">
                         <span className="p-0.5">
                           {selectedProduct?.dimensions.width}
                         </span>
                       </div>
-                      <div className="w-9 h-8 p-1 flex justify-center items-center bg-blue-300 dark:bg-blue-500 mr-2 text-sm font-semi-bold">
+                      <div className="w-9 h-8 p-1 flex justify-center items-center bg-blue-300 rounded-lg mr-2 text-sm font-semi-bold hover:scale-125 tranisition ease-in-out">
                         <span className="p-0.5">
                           {selectedProduct?.dimensions.height}
                         </span>
                       </div>
-                      <div className="w-9 h-8 p-1 flex justify-center items-center bg-yellow-500 dark:bg-yellow-700 mr-2 text-sm font-semi-bold">
+                      <div className="w-9 h-8 p-1 flex justify-center items-center bg-yellow-500 rounded-lg mr-2 text-sm font-semi-bold hover:scale-125 tranisition ease-in-out">
                         <span className="p-0.5">
                           {selectedProduct?.dimensions.depth}
                         </span>
@@ -137,14 +147,13 @@ export const SingleProduct: React.FC = () => {
                       <div className="bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">
                         {selectedProduct?.warrantyInformation}
                       </div>
-                     
                     </div>
                   </div>
                   <div>
                     <span className="font-bold text-gray-700 dark:text-gray-300">
                       Product Description:
                     </span>
-                    <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                    <p className="text-gray-600 dark:text-gray-300 text-md font-semibold mt-2">
                       {selectedProduct?.description}
                     </p>
                   </div>
