4b31e2590f9e635b379756202b24e9add6d65eee
Sarika <sarika.goooogle.1330@gmail.com>
2024-06-05 22:04:47 +0530

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ export const Navbar = () => {
     <>
       <nav className="bg-[#e7ebee] shadow shadow-gray-300 w-100 px-8 md:px-auto">
         <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
-          <Link to="/">
-            <div className="hidden lg:flex md:flex lg:flex-row lg:w-[6%] md:w-[8%] md:flex-row ">
-              <img className="w-full" src={logo} alt="logo" />
+          
+            <div className="hidden lg:flex md:flex lg:flex-row lg:w-[6%] md:w-[7%] md:flex-row ">
+              <Link to="/"><img className="w-3/4" src={logo} alt="logo" /></Link>
             </div>
-          </Link>
+          
           <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
             <ul className="flex font-semibold justify-between">
               <Link to="/">
