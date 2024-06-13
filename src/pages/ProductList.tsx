17d9d578a2532be0b787c52a8c49de03c2cba9b1
Sarika <sarika.goooogle.1330@gmail.com>
2024-06-13 17:32:59 +0530

    updated data

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -8,25 +8,17 @@ import { SearchBar } from "../components/SearchBar";
 import { Product } from "../interfaces/product";
 import { Link } from "react-router-dom";
 import { SingleProCard } from "../components/SingleProCard";
-import axios from "axios";
 
 const ProductList: React.FC = () => {
-  const [tempData,setTempData]=useState<Product[]>([])
   const dispatch = useDispatch<AppDispatch>();
   const products = useSelector(selectAllProducts);
   const productStatus = useSelector(
     (state: RootState) => state.products.isLoading
   );
-  
+
   const [currentPage, setCurrentPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
   const productsPerPage = 8;
-
-  useEffect(()=>{
-    axios.get("https://sarikasingh30.github.io/data-api-agent/db.json").then((res)=>setTempData(res.data.products.slice(0,8)))  
-  },[])
-
-
 
   useEffect(() => {
     if (productStatus === true) {
@@ -59,16 +51,15 @@ const ProductList: React.FC = () => {
   return (
     <>
       {productStatus ? (
-        <div className="mt-5 flex justify-center items-center">
-          <div className="p-1 m-auto grid grid-cols-1 auto-rows-auto lg:grid-cols-4 md:grid-cols-2">
-              {tempData?.map((el, index) => {
-                return (
-                  <Link to={`/product/${el.id}`} key={el.id}>
-                    <SingleProCard prod={el} index={index}/>
-                  </Link>
-                );
-              })}
-            </div>
+        <div className="w-full h-screen flex flex-row justify-center items-center">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-warning motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
         </div>
       ) : (
         <div className="w-[90%] m-auto">
@@ -78,7 +69,7 @@ const ProductList: React.FC = () => {
               {filteredProducts?.map((el, index) => {
                 return (
                   <Link to={`/product/${el.id}`} key={el.id}>
-                    <SingleProCard prod={el} index={index}/>
+                    <SingleProCard prod={el} index={index} />
                   </Link>
                 );
               })}
